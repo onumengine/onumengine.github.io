37eaf81489b6c50ae009644a5a7bf311de341766
onumengine <onumdev@gmail.com>
2021-02-19 03:05:11 +0100

    added the about section

--- script.js
+++ script.js
@@ -4,19 +4,36 @@ function main() {
     hideAllTabs();
     deactivateTabItems();
     openMainPage();
+
+    var aboutButton = document.getElementById("aboutButton");
+    aboutButton.onclick = showAboutSection;
+    
+    var homeButton = document.getElementById("homeButton");
+    homeButton.onclick = showMainPage;
+}
+
+function showAboutSection() {
+    var aboutSection = document.getElementById("aboutSection");
+    var tabContents = document.getElementsByClassName("tabContent");
+    for (let i = 0; i < tabContents.length; i++) {
+        tabContents[i].style.display = "none";
+    }
+    aboutSection.style.display = "block"
+}
+
+function showMainPage() {
+    var mainPage = document.getElementById("mainPage");
+    var tabContents = document.getElementsByClassName("tabContent");
+    for (let i = 0; i < tabContents.length; i++) {
+        tabContents[i].style.display = "none";
+    }
+    mainPage.style.display = "block"
 }
 
 function assignHomeButton() {
     homeButton = document.getElementById("homeButton");
     homeButton.onclick = () => {
         openPage("homeButton");
-    }
-}
-
-function hideAllTabs() {
-    let tabContents = document.getElementsByClassName("tabContent");
-    for (let content of tabContents) {
-        content.style.display = "none";
     }
 }
 
@@ -27,11 +44,18 @@ function deactivateTabItems() {
     }
 }
 
-function openTab(clickEvent, tabName) {
-    document.getElementById(tabName).style.display = "block";
-    clickEvent.currentTarget.className += " active";
+function hideAllTabs() {
+    let tabContents = document.getElementsByClassName("tabContent");
+    for (let content of tabContents) {
+        content.style.display = "none";
+    }
 }
 
 function openMainPage() {
     document.getElementById("mainPage").style.display = "block";
+}
+
+function openTab(clickEvent, tabName) {
+    document.getElementById(tabName).style.display = "block";
+    clickEvent.currentTarget.className += " active";
 }
